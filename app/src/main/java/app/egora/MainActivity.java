2d40f6e822dcc2f099d92976e8ad178a6db62dd9
Nina <tietje.hh@gmail.com>
2019-02-19 23:58:23 +0100

    working firebase auth registration in MainAcivity.java

--- app/src/main/java/app/egora/MainActivity.java
+++ app/src/main/java/app/egora/MainActivity.java
@@ -1,13 +1,75 @@
 package app.egora;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonReg;
+    private Button buttonUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonReg = findViewById(R.id.buttonRegTest);
+        buttonUser = findViewById(R.id.buttonUser);
+
+        buttonUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getState();
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser("nn@gmail.com", "han2200nn");
+                Log.i("register", "user registered");
+            }
+        });
     }
+
+    private void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i("reg", "reg complete");
+                }
+                else {
+                    Log.i("reg", "reg failed", task.getException());
+                }
+                getState();
+            }
+        });
+    }
+
+    private void getState() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.i("user", "current user is: " + currentUser.getUid());
+        }
+        else {
+            Log.i("user", "user is null");
+        }
+    }
+
 }
